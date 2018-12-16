30a279cc9f91d8e58724b1e967f86f5b008f16c4
wara_1234 <wadh1n1@hotmail.com>
2018-12-16 18:29:10 +0700

    Add all knowledge base
    This consists of 3 main parts
    1) port_locations -> all ports location stays here
    2) port_distance -> specify edges connected to each node with its cost for travelling
    3) astar-algorithm -> this is where the a* algorithm is allocated

---
+++ port_locations.pl
@@ -0,0 +1,75 @@
+% ports - brunei darussalam
+location(brunei_darussalam, kualabelait, 4.633333333, 114.20000000).
+location(brunei_darussalam, muara,  5.016666667, 115.0666667).
+
+% ports - cambodia
+location(cambodia, kompongsom, 10.63333333, 103.50000000).
+location(cambodia, phnompenh, 11.58333333, 104.9166667).
+
+%ports - indonesia
+location(indonesia, cilacap, 8.733333333, 109.00000000).
+location(indonesia, cirebon, 6.683333333, 108.55000000).
+location(indonesia, jakarta, 6.10000000, 106.86666666).
+location(indonesia, kupang, 10.166666667, 123.56666667).
+location(indonesia, palembang, 2.98333333, 104.75000000).
+location(indonesia, semarang, 6.88333333, 110.40000000).
+location(indonesia, surabaya, 7.216666667, 112.7333333).
+location(indonesia, ujungpandang, 5.133333333, 119.4).
+
+% ports - malaysia
+location(malaysia, bintulu, 3.266666667, 112.9833333).
+location(malaysia, kotakinabalu, 5.983333333, 116).
+location(malaysia, kuantan, 3.966666667, 103.4333333).
+location(malaysia, kuching,1.566666667, 110.35 ).
+location(malaysia, kudat, 6.883333333, 116.85).
+location(malaysia, labuan, 5.283333333, 115.25).
+location(malaysia, lahaddatu, 5.033333333, 118.3333333).
+location(malaysia, lumut, 4.266666667, 100.65).
+location(malaysia, miri, 4.433333333,  113.9166667).
+location(malaysia, pasirgudang, 1.433333333, 103.90000000).
+location(malaysia, penang(georgetown), 5.416666667, 100.35).
+location(malaysia,portdickson, 2.516666667, 101.7833333).
+location(malaysia, portklang(kelang), 3.00000000, 101.40000000).
+location(malaysia, sandakan, 5.833333333, 118.1166667).
+location(malaysia, sibu, 2.30000000, 111.8166667).
+location(malaysia, tanjungpelepas, 1.35, 103.5333333).
+location(malaysia, tawau,  4.233333333, 117.8666667).
+
+%ports - myanmar
+location(myanmar, bassein, 16.75, 94.71666667).
+location(myanmar, moulmein, 16.48333333, 97.61666667).
+location(myanmar, yangon, 16.78333333, 96.25).
+
+% ports - philippines
+location(philippines, batangas, 13.75, 121.05).
+location(philippines, cagayandeoro, 8.5, 124.65).
+location(philippines, cebu, 10.3, 123.8833333).
+location(philippines, davao, 7.066666667, 125.6333333).
+location(philippines, iligan, 8.25, 124.2).
+location(philippines, iloilo, 10.68333333, 122.5833333).
+location(philippines, jolo, 6.05, 121).
+location(philippines, legaspi, 13.15, 123.75).
+location(philippines, manila, 14.58333333, 120.9666667).
+location(philippines, puertoprincesa, 9.733333333, 118.7166667).
+location(philippines, sanfernando, 16.61666667, 120.3).
+location(philippines, subicbay, 14.81666667, 120.3).
+location(philippines, zamboanga, 6.9, 122.0666667).
+
+% ports - singapore
+location(singapore, singapore, 1.266666667, 103.8333333).
+
+% ports - thailand
+location(thailand, bangkok, 13.43333333, 100.5833333).
+location(thailand, laemchabang, 13.08333333, 100.8833333).
+location(thailand, pattani, 6.95, 101.3).
+location(thailand, phuket, 7.816666667, 98.4).
+location(thailand, sattahip, 12.58333333, 100.9166667).
+location(thailand, songkhla, 7.233333333, 100.5833333).
+location(thailand, sriracha, 13.11666667, 100.8666667).
+
+% ports - vietnam
+location(vietnam, danang ,16.1 , 108.3).
+location(vietnam, haiphong, 20.86666667,106.6833333 ).
+location(vietnam, hochiminhcity, 10.83333333,106.75 ).
+location(vietnam, hongay, 20.95, 107.1).
+location(vietnam, nhatrang, 12.2, 109.2166667).
